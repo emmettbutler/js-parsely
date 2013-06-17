fa228864ed72e43abfa8ebc7f9c5c9b064fc3a67
emmett9001 <ebutler9@gmail.com>
2013-06-17 08:47:42 -0400

    so many callbacks..... T^T

--- parsely.js
+++ parsely.js
@@ -6,12 +6,22 @@ var Parsely = function(){
     var get_public_key = function(){ return public_key; }
     var get_secret_key = function(){ return secret_key; }
 
-    var authenticate = function(publickey, secretkey){
+    var authenticate = function(publickey, secretkey, callback){
         public_key = publickey;
         secret_key = secretkey;
+
+        _request_endpoint('/analytics/posts', {}, function(data){
+            if(callback != undefined){
+                if(data.code == 403 && data.success == false){
+                    callback(false);
+                } else {
+                    callback(true);
+                }
+            }
+        });
     };
 
-    var _request_endpoint = function(endpoint, options){
+    var _request_endpoint = function(endpoint, options, callback){
         var url = root_url + endpoint + "?apikey=" + public_key + "&";
         url += "secret=" + secret_key + "&";
         for (var key in options) {
@@ -22,22 +32,18 @@ var Parsely = function(){
             }
         }
 
-        var script = document.createElement("script");
-        script.src = url + "callback=parsely.jsonp_callback&";
-        console.log(url);
-        document.getElementsByTagName("head")[0].appendChild(script);
-        return "butt";
-    };
+        var callback_name = '_parsely_callback';
+        // set up the jsonp callback
+        window[callback_name] = function(reply){ callback(reply); };
 
-    var _jsonp_callback = function(data){
-        console.log(data);
-        console.log("JSONP happened");
+        var script = document.createElement("script");
+        script.src = url + "callback=" + callback_name + "&";
+        document.getElementsByTagName("head")[0].appendChild(script);
     };
 
     return {
         authenticate: authenticate,
         request_endpoint: _request_endpoint,
-        jsonp_callback: _jsonp_callback,
 
         public_key: get_public_key,
         secret_key: get_secret_key,
--- test.parsely.js
+++ test.parsely.js
@@ -6,19 +6,37 @@ var parsely = new Parsely();
 
 describe('Parsely', function() {
   describe('authenticate()', function() {
-    it('should set public key', function() {
-      assert.equal(parsely.public_key(), publickey);
+    it('should set public key', function(done) {
+      this.timeout(10000);
+      parsely.authenticate(publickey, secretkey, function(success){
+        assert.equal(parsely.public_key(), publickey);
+        done();
+      });
     })
-    it('should set secret key', function() {
-      assert.equal(parsely.secret_key(), secretkey);
+    it('should set secret key', function(done) {
+      this.timeout(10000);
+      parsely.authenticate(publickey, secretkey, function(success){
+        assert.equal(parsely.secret_key(), secretkey);
+        done();
+      });
+    })
+    it('should fail on bad keys', function(done){
+      this.timeout(10000);
+      parsely.authenticate(publickey, "dummy", function(success){
+        assert.isFalse(success);
+        done();
+      })
     })
   })
 
   describe('request_endpoint()', function(){
-    parsely.authenticate(publickey, secretkey);
-    var res = parsely.request_endpoint('/analytics/posts', {})
-    it('should get some JSON', function(){
-      assert.isTrue(res.hasOwnProperty("data") && res != undefined);
+    it('should get some JSON', function(done){
+      this.timeout(10000);
+      parsely.authenticate(publickey, secretkey, function(success){});
+      parsely.request_endpoint('/analytics/posts', {}, function(res){
+        assert.isTrue(res != undefined && res.hasOwnProperty('data'));
+        done();
+      })
     })
   })
 })
